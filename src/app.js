0ba0013f006a87b145a6deadd98d3e5e07390b71
mustafasatiroglu <mmustafa.satiroglu@gmail.com>
2021-10-19 14:38:53 +0300

    18 Ekim 2021- Js Work Week1

---
+++ src/app.js
@@ -0,0 +1,11 @@
+ 
+// Array
+
+let konutKredileri = ["Konut Kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","Banka Konut Kredisi",222,['Marek Hamsik',"Bakasetas"]]
+//console.log(konutKredileri)
+
+console.log("<ul>")
+for(let i = 0; i<konutKredileri.length; i++){
+    console.log("<li>" + konutKredileri[i] + "</li>")
+}
+console.log("</ul>")
